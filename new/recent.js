adf8ca599d33b640e164e84bd397c87c45e3311d
PRONOB BISWAS <biswaspronob@outlook.com>
2024-02-10 04:22:19 +0600

    update

--- new/recent.js
+++ new/recent.js
@@ -2,12 +2,12 @@ let element = document.getElementById("div1");
 element.style.width = "400px";
 element.style.height = "400px";
 element.style.backgroundColor = "whitesmoke"
-
+// =======prgraph=========
 const para = document.createElement("p");
 const mynode = document.createTextNode("This is new.");
 para.appendChild(mynode);
 element.appendChild(para);
-
+// ===========heading==========
 const headdding = document.createElement("h1");
 headdding.style.color= "red"
 const secondNode = document.createTextNode("hello");
@@ -37,7 +37,97 @@ pic.alt = "photos";
 pic.src = "./pic.jpg"
 element.appendChild(pic);
 
+// ===========next stape=======
+let person = [];
+ person1 = {
+    name : "pronob biswas",
+    batch : 2306,
+    program : "MERN stack"
+};
+person.push(person1)
+person2 = {
+    name : "dipto vai",
+    batch : 2306,
+    program : "MERN stack"
+};
+person.push(person2);
+person3 = {
+    name : "arup dhar",
+    batch : 2306,
+    program : "MERN stack"
+};
+person.push(person3);
+person4 = {
+    name : "bejoy vai",
+    batch : 2306,
+    program : "MERN stack"
+};
+person.push(person4);
+console.log(person);
 
+// ==============asimple constractor function=========
+function secondPerson(names,batch,depertment,gitlink){
+    this.names = names;
+    this.batch = batch;
+    this.depertment = depertment;
+    this.gitlink = gitlink;
+};
+
+const ami = new secondPerson ("pronob" , 2306 , "MERN", "github.com");
+const dipto = new secondPerson ("dipto" , 2306 , "MERN", "github.com");
+const saykat = new secondPerson("saykat", "####" , "radiology" ,"github.com");
+
+console.log(ami);
+console.log(saykat);
+
+let I = saykat.names;
+const h4 = document.createElement("h4");
+const secondText = document.createTextNode(I);
+h4.appendChild(secondText);
+element.appendChild(h4)
+
+
+// ===========second division=============
+
+let section = document.querySelector("#secndSection"); //maindiv
+section.classList.add('section2nd');
+
+let flexbox = document.createElement('div');//flexbox
+flexbox.classList.add('flexbox');
+flexbox.innerHTML = `<h1>hello</h1>`;
+
+let formBox =  document.createElement('div');
+formBox.classList.add('formBox');
+flexbox.appendChild(formBox);
+// =====this the form=====
+let forms =  document.createElement('form');
+forms.classList.add('formBox');
+formBox.appendChild(forms);
+// =====input type text=====
+let inputs =  document.createElement('input');
+inputs.classList.add('formBox');
+inputs.type = "text";
+forms.appendChild(inputs);
+// =======the button elements====
+let doBtn = document.createElement('button');
+doBtn.innerText = "click me";
+forms.appendChild(doBtn);
+// ========output result========
+let toDo = document.createElement('div');
+formBox.appendChild(toDo);
+// =======******form Input Value******=======
+// =======******form Input Value******=======
+toDo.addEventListener("click" , function(){
+    let inputValue = inputs.value;
+    toDo.innerText = inputValue;
+    console.log(inputs.value);
+})
+
+
+
+
+
+section.appendChild(flexbox);
 
 
 
